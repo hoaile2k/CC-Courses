e482b6fba2e98af389a4cdbbc82d5cd640aefd82
Hoai Le <lvhoaijob@gmail.com>
2022-05-12 16:32:06 +0700

    update can use two key once

--- Chapter07/assets/Script/RipControl.js
+++ Chapter07/assets/Script/RipControl.js
@@ -14,8 +14,8 @@ cc.Class({
     onLoad () {
         let manager = cc.director.getCollisionManager()
         manager.enabled = true
-        manager.enabledDebugDraw = true
-        manager.enabledDrawBoundingBox = true
+        // manager.enabledDebugDraw = true
+        // manager.enabledDrawBoundingBox = true
     },
 
     start () {
